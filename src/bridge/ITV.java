1568526ca9a13f63186ccc0cf7da501cc028396c
Huang Lei <leihuang@ecarx.com.cn>
2016-12-26 20:39:05 +0800

    桥接
    Signed-off-by: Huang Lei <leihuang@ecarx.com.cn>

---
+++ src/bridge/ITV.java
@@ -0,0 +1,11 @@
+package bridge;
+
+public interface ITV {
+	
+	public void on();
+
+	public void off();
+
+	public void switchChannel(int channel);
+	
+}
---
+++ src/bridge/SamsungTV.java
@@ -0,0 +1,20 @@
+package bridge;
+
+public class SamsungTV implements ITV {
+	
+	@Override
+	public void on() {
+		System.out.println("Samsung is turned on.");
+	}
+
+	@Override
+	public void off() {
+		System.out.println("Samsung is turned off.");
+	}
+
+	@Override
+	public void switchChannel(int channel) {
+		System.out.println("Samsung: channel - " + channel);
+	}
+	
+}
