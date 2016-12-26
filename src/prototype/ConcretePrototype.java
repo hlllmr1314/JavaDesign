697472b5bed9bf974d997b64ae05b19dd47dfe31
Huang Lei <leihuang@ecarx.com.cn>
2016-12-26 17:30:27 +0800

    原型模式
    Signed-off-by: Huang Lei <leihuang@ecarx.com.cn>

---
+++ src/prototype/ConcretePrototype.java
@@ -0,0 +1,14 @@
+package prototype;
+
+/**
+ * 
+ * @author huanglei
+ *
+ */
+public class ConcretePrototype extends Prototype {
+	
+	public ConcretePrototype(String name) {
+		setName(name);
+	}
+	
+}
---
+++ src/prototype/Test.java
@@ -0,0 +1,18 @@
+package prototype;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		try {
+			Prototype pro = new ConcretePrototype("prototype");
+			Prototype pro2 = (Prototype) pro.clone();
+			System.out.println(pro.getName());
+			System.out.println(pro2.getName());
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
